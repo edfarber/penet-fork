e8acd8c8ab2496e2a97975bba60f9f442b15c291
edf1247 <edf1247@gmail.com>
2025-11-19 21:44:53 -0500

    t

--- tune.sh
+++ tune.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 #SBATCH -N 1
-#SBATCH -n 4
+#SBATCH -n 16
 #SBATCH --mem=64G
 #SBATCH -t 6:00:00
 #SBATCH -p gpu --gres=gpu:1
 #SBATCH -o /users/edfarber/tune.log
 
-/users/edfarber/miniconda3/envs/penet_new/bin/python scripts/hparam_search.py \
+/users/edfarber/scratch/penet-fork/.venv/bin/python scripts/hparam_search.py \
   --data_dir /users/edfarber/scratch/dataset/multimodalpulmonaryembolismdataset/0/ \
   --dataset pe \
   --name penet_search \
   --storage sqlite:///penet_optuna.db \
   --study_name penet_optuna \
   --trials 50 \
-  --search_epochs 40 \
-  --num_workers 4 \
+  --search_epochs 5 \
+  --num_workers 16 \
   --direction maximize
