1b9ca9010c2ecc3540ea791a145df681a36ba065
edf1247 <edf1247@gmail.com>
2025-11-20 03:06:32 -0500

    fixed tesing

--- test.sh
+++ test.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 #SBATCH -N 1
-#SBATCH -n 4
+#SBATCH -n 1
+#SBATCH -c 8
 #SBATCH --mem=64G
 #SBATCH -t 4:00:00
 #SBATCH -p gpu --gres=gpu:1
 #SBATCH -o /users/edfarber/test.log
 
-python test.py --data_dir /users/edfarber/scratch/dataset/multimodalpulmonaryembolismdataset/0/ \
-                 --ckpt_path /users/edfarber/train_logs/PositiveWeighting_20251113_132622/best.pth.tar \
-                 --results_dir results \
-                 --phase test \
-                 --name test2 \
-                 --dataset pe \
-                 --gpu_ids 0
+# Path to your python environment
+PYTHON_EXEC=/users/edfarber/scratch/penet-fork/.venv/bin/python
+
+# Path to your data
+DATA_DIR=/users/edfarber/scratch/dataset/multimodalpulmonaryembolismdataset/0/
+
+# Path to the checkpoint you want to test
+# IMPORTANT: Update this to point to the specific checkpoint you want to evaluate!
+CKPT_PATH=/users/edfarber/train_logs/Baseline_20251113_034716/best.pth.tar
+
+# Run the test using the config wrapper to ensure correct preprocessing
+$PYTHON_EXEC scripts/run_test_with_config.py train_configs.json \
+    --ckpt_path $CKPT_PATH \
+    --data_dir $DATA_DIR \
+    --phase test
